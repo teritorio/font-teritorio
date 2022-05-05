523c80456dc6ba0bbdf0a1c7d90e48eb36f44cbb
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-05-05 11:39:34 +0200

    Simplify flat icon

--- scripts/flat_ico.sh
+++ scripts/flat_ico.sh
@@ -6,9 +6,6 @@ cd icones:pictos:vectoriel
 find . -name "*.svg" | while read ico; do
   echo "$ico"
   OUT=${ico#.\/}
-  SUPER_CLASS=${OUT%\/*}
-  SUPER_CLASS=${SUPER_CLASS%.*}
-  SUPER_CLASS=${SUPER_CLASS%\/*}
   OUT=${OUT//\//-}
 
   WH=`identify -format '%w-%h' "${ico}"`
@@ -21,9 +18,5 @@ find . -name "*.svg" | while read ico; do
     RESIZE_11="-w 11"
   fi
 
-  OUT_B=${OUT}
-  rsvg-convert --keep-aspect-ratio $RESIZE_11 -f svg "$ico" -o "../svg/$OUT_B"
-  mv "../svg/$OUT_B" "../svg/_$OUT_B"
-  ruby ../scripts/resize.rb "../svg/_$OUT_B" > "../svg/$OUT_B"
-  rm "../svg/_$OUT_B"
+  rsvg-convert --keep-aspect-ratio $RESIZE_11 -f svg "$ico" -o "../svg/$OUT"
 done
